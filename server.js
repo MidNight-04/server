cd0e78d9df328bbd08818de4a069fdfc50eb276f
Vikas Raghav <97186993+MidNight-04@users.noreply.github.com>
2025-02-13 12:52:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ require("./app/routes/task.routes")(app);
 require("./app/routes/taskCategory.routes")(app);
 require("./app/routes/projectLog.routes")(app);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

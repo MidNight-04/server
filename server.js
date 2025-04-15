c7b43180bb0fdff5c79d2a3a86831ae4982a3c36
Vikas Raghav <97186993+MidNight-04@users.noreply.github.com>
2025-04-15 15:48:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -52,7 +52,8 @@ const dbConfig = require('./app/config/db.config');
 db.mongoose
   .connect(
     // `mongodb+srv://${dbConfig.HOST}:${dbConfig.PORT}@${dbConfig.url}.mongodb.net/${dbConfig.DB}`,
-    `mongodb+srv://${process.env.MONGODB_HOST}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}.mongodb.net/${process.env.MONGODB_DB}`,
+    // `mongodb+srv://${process.env.MONGODB_HOST}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}.mongodb.net/${process.env.MONGODB_DB}`,
+        `mongodb+srv://vikasraghavthikedaar:Qaz_7410@cluster0.enllx.mongodb.net/bldox`,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
